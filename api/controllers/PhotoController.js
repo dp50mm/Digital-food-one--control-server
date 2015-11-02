a41fc23bd696c5fd12cd41635b338c16ab9fcb5f
Erwin <e.hoogerwoord@gmail.com>
2015-11-02 23:26:17 +0100

    1

--- api/controllers/PhotoController.js
+++ api/controllers/PhotoController.js
@@ -7,12 +7,12 @@
 
 module.exports = {
 	index: function(req, res) {
-		console.log(Photo);
-		Photo.find({name:'test test'}).exec(function findCB(err, found) {
-			while(found.length) {
-				console.log('Found photo with name: '+found.pop().name);
-			}
-		});
+		console.log(Photo.connections.productionMysql);
+		// Photo.find({name:'test test'}).exec(function findCB(err, found) {
+		// 	while(found.length) {
+		// 		console.log('Found photo with name: '+found.pop().name);
+		// 	}
+		// });
 		// Photo.create({name:'test test'}).exec(function createPhoto(err, created) {
 		// 	console.log('created photo with name: '+created.name);
 		// });
